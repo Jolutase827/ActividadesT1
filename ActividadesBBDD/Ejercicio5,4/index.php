c4f7c12a68613fba07a4ffdac530252eb8a887ee
Jose Luis <jolutase104@gmail.com>
2023-10-19 18:59:33 +0200

    5,5 creado

--- ActividadesBBDD/Ejercicio5,4/index.php
+++ ActividadesBBDD/Ejercicio5,4/index.php
@@ -1,20 +1,16 @@
-<?php 
+<?php
 require "model.php";
 $base = new Base();
-if(isset($_POST["button"])) {
-    $pedido = new Pedido($_POST["idPedido"],$_POST['fecha'],$_POST["cliente"]);
-    if($pedido->isPedido($base->link)){
-        $pedido->addLinea($base->link,$_POST["producto"],$_POST['cantidad']);
+if (isset($_POST["button"])) {
+    $pedido = new Pedido($_POST["idPedido"], $_POST['fecha'], $_POST["cliente"]);
+    if ($pedido->isPedido($base->link)) {
+        $pedido->addLinea($base->link, $_POST["producto"], $_POST['cantidad']);
         echo "Se ha añadido la line con exito";
-    }else{
+    } else {
         $pedido->createPedido($base->link);
-        $pedido->addLinea($base->link,$_POST["producto"],$_POST['cantidad']);
+        $pedido->addLinea($base->link, $_POST["producto"], $_POST['cantidad']);
         echo "Se ha añadido la linea y el pedido con exito";
     }
 } else {
     require "views/viewForm.php";
 }
-
-
-
-?>
